75f7a83cdf130f60884d374b5f79e83dc683c6d8
Guh90 <57641874+Guh90@users.noreply.github.com>
2025-06-13 11:46:59 -0400

    feat Switch Case

---
+++ operadores_logicos/Lacos_Do_For/Switch_Case.js
@@ -0,0 +1,20 @@
+var dia="Quarta";
+switch (dia) {
+    case "Segunda":
+        console.log("Dia de começar a semana");
+        break;
+    case "Terça":
+        console.log("Hora de aprender coisasa novas");
+        break;
+    case "Quarta":
+        console.log("Metade da semana, comntinue firme");
+    break;
+    case "Quinta";
+    console.log("Quase lá, mais um pouco de esforço");
+    break;
+    case "Sexta":
+        console.log("Dia de relaxar, o final de semana chegou");
+        break;
+    default:
+    console.log("Final de semana chegou");
+}
