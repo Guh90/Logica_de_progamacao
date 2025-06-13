67aead25402e7f6e731810474a56ddd36a613300
Guh90 <57641874+Guh90@users.noreply.github.com>
2025-06-13 10:23:17 -0400

    feat Laços

--- operadores_logicos/Lacos.js
+++ operadores_logicos/Lacos.js
@@ -1,3 +1,4 @@
 var numeros;
-do {numeros = prompt ("Digite um numero entre 1 e 10;");} while ( numeros < 1 || numeros > 10);
-console.log(`você digitou o numero ${numeros}.`);
+do {numeros = prompt ("Digite um numero entre 1 e 10;");
+    } while ( numeros < 1 || numeros > 10);
+console.log(`Você digitou o numero ${numeros}.`);
