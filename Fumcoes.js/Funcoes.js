06dfa8306ac5b9bba41b48f76e5f91a4c6eff969
Guh90 <57641874+Guh90@users.noreply.github.com>
2025-06-16 11:18:17 -0400

    Update Funcoes.js

--- Fumcoes.js/Funcoes.js
+++ Fumcoes.js/Funcoes.js
@@ -1,6 +1,12 @@
-function dobrar (num) { valor = num *2;
+/*function dobrar (num) { valor = num *2;
     return num;
 }
 var valor = 10;
 console.log(dobrar (valor)); //20
-console.log(valor); // 10 (valor original não mudou)
+console.log(valor); // 10 (valor original não mudou)*/
+
+function saudacao(nome = "visitante") {
+    console.log(`Olá, ${nome}!`);
+}
+saudacao(); // Olá, visitante!
+saudacao("Gusttavo"); // Olá, Gusttavo!
